ceb474d3c189f4b7dd19f855d1205e167cc392f5
TheeLiFang <theelifang@gmail.com>
2017-12-23 01:28:23 +0800

    Add files via upload

---
+++ application/views/admin/product/productdetail.php
@@ -0,0 +1,112 @@
+<div class="compare-area compare-single-productt">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="single-thumbnail-wrapper">
+                            <div class="single-product-tab"></div>
+                            <div class="single-cat-main">
+                                <div class="tab-content">
+                                    <div role="tabpanel" class="tab-pane active" id="tab-1">
+                                        <div class="tab-single-image">
+                                            <a href="images/product/p1.jpg" class="fancybox" data-fancybox-group="gallery"><img src="images/product/p1.jpg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane" id="tab2">
+                                        <div class="tab-single-image">
+                                            <a href="images/product/p2.jpg" class="fancybox" data-fancybox-group="gallery"><img src="images/product/p2.jpg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane" id="tab3">
+                                        <div class="tab-single-image">
+                                            <a href="images/product/p3.jpg" class="fancybox" data-fancybox-group="gallery"><img src="images/product/p3.jpg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane" id="tab4">
+                                        <div class="tab-single-image">
+                                            <a href="images/product/p4.jpg" class="fancybox" data-fancybox-group="gallery"><img src="images/product/p4.jpg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="compare-content-wrap">
+                            <div class="cmain-heading text-uppercase">
+                                <h3>Product Name</h3>
+                            </div>
+                            <div class="compare-conpart compare-single-mgr">
+                                <div class="skill-referance">
+                                    <p> Reference: demo_12</p>
+                                    <p><span class="italic">Condition:</span> New product</p>
+                                </div>
+                            </div>
+                            <div class="compare-conpart compare-conpart-text">
+                                <div class="skill-long-text">
+                                    <p>Product Details.</p>
+                                </div>
+                            </div>
+                            <div class="compare-conpart-pm compare-bottom">
+                                <div class="old-new-price">
+                                    <span>RM 342.00 <del class="skill-gray"> RM 360.00</del></span>
+                                </div>
+                                <div class="plus-minus-text">Quantity</div>
+                                <div class="skill-plusminus-wrap">
+                                    <div class="skill-plusminus">
+                                        <div class="skill-minus qtybutton">-</div>
+                                        <input type="text" name="#" value="1" class="cart-plus-minus-box" />
+                                        <div class="skill-plus qtybutton">+</div>
+                                    </div>
+                                </div>
+                                <div class="skill-cart-option skill-sep posr">
+                                    <a href="cart.html"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                </div>
+                            </div>
+                            <div class="compare-conpart skill-communicate">
+                                <div class="skill-checklist">
+                                    <label for="skillc"><span class="italic">Size</span>
+                                    </label>
+                                    <select id="skillc">
+                                        <option value="">S</option>
+                                        <option value="">M</option>
+                                        <option value="">L</option>
+                                    </select>
+                                </div>
+
+                            </div>
+                            <div class="color-instock">
+                                <div class="skill-colors">
+                                    <span class="italic">Color</span>
+                                    <ul class="skill-ulli">
+                                        <li class="light-black click-bdr">
+                                            <a href="#"></a>
+                                        </li>
+                                        <li class="skill-orange">
+                                            <a href="#"></a>
+                                        </li>
+                                        <li class="skill-blue">
+                                            <a href="#"></a>
+                                        </li>
+                                        <li class="skill-yellow">
+                                            <a href="#"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="compare-conpart-items">
+                                    <div class="skill-long-items">
+                                        <p><span class="italic skill-items">90 Items</span><span class="skill-instock"> In Stock</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
